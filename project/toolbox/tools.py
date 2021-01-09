01678d1c8608295e58633d94544ba0b0290493a4
Semmilhe <70484761+Semmilhe@users.noreply.github.com>
2021-01-09 19:48:35 +0100

    gridsearch functie en implementatie

--- project/toolbox/tools.py
+++ project/toolbox/tools.py
@@ -30,28 +30,23 @@ def k_fold(X_data, y, folds, start, stop, step, method):
         for train, test in kf.split(X_data, y):
             
             if(method == 'tree'):
-                split_clf = tree.DecisionTreeClassifier(criterion='gini', max_depth = i)
-                split_clf = split_clf.fit(X_data[train], y[train])
-                accuracy_train = np.append(
-                    accuracy_train, metrics.accuracy_score(y[train], split_clf.predict(X_data[train])))
-                accuracy_test = np.append(
-                    accuracy_test, metrics.accuracy_score(y[test], split_clf.predict(X_data[test]))) 
+                clf = tree.DecisionTreeClassifier(criterion='gini', max_depth = i)
             
             elif(method == 'rf'):
-                rf = RandomForestClassifier(n_estimators = 100, criterion='gini', max_depth = i)
-                rf = rf.fit(X_data[train], y[train])
-                accuracy_train = np.append(
-                    accuracy_train, metrics.accuracy_score(y[train], rf.predict(X_data[train])))
-                accuracy_test = np.append(
-                    accuracy_test, metrics.accuracy_score(y[test], rf.predict(X_data[test]))) 
-                
+                clf = RandomForestClassifier(n_estimators = 100, criterion='gini', max_depth = i)
+               
             elif(method == 'rf_estimators'):
-                    rf = RandomForestClassifier(n_estimators = i, criterion='gini', max_depth = 4)
-                    rf = rf.fit(X_data[train], y[train])
-                    accuracy_train = np.append(
-                        accuracy_train, metrics.accuracy_score(y[train], rf.predict(X_data[train])))
-                    accuracy_test = np.append(
-                        accuracy_test, metrics.accuracy_score(y[test], rf.predict(X_data[test]))) 
+                clf = RandomForestClassifier(n_estimators = i, criterion='gini', max_depth = 4)
+        
+            elif(method == 'xgb'):
+                clf = xgb.XGBClassifier(n_estimators = 100, max_depth = i, eval_metric='error', use_label_encoder=False)
+                
+            elif(method == 'xgb_estimators'):
+                clf = xgb.XGBClassifier(n_estimators = i, max_depth = 5, eval_metric='error', use_label_encoder=False)
+            
+            clf = clf.fit(X_data[train], y[train])
+            accuracy_train = np.append(accuracy_train, metrics.accuracy_score(y[train], clf.predict(X_data[train])))
+            accuracy_test = np.append(accuracy_test, metrics.accuracy_score(y[test], clf.predict(X_data[test]))) 
                 
         accuracy_mean_train = np.append(accuracy_mean_train, np.mean(accuracy_train))
         accuracy_mean_test = np.append(accuracy_mean_test, np.mean(accuracy_test))
