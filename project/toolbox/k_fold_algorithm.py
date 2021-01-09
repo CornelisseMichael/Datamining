8604cf4b187d7b875a5fe0d62f953311bf75ef26
Michael Cornelisse <michaelcornelisse88@gmail.com>
2021-01-09 21:20:57 +0100

    updated code and comments

--- project/toolbox/k_fold_algorithm.py
+++ project/toolbox/k_fold_algorithm.py
@@ -1,12 +1,33 @@
-
+import os
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
 from sklearn.model_selection import StratifiedKFold    
 from sklearn import metrics
 from sklearn import tree    
 from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
 import xgboost as xgb
-import numpy as np
-import matplotlib.pyplot as plt
+
 def k_fold(X_data, y, folds, start, stop, step, method):
+    """
+    Returns accuracy mean for train and test data
+    Parameters
+    ----------
+    X_data : 
+        train data
+    y_np : numpy array
+        test data
+    fold : int
+        number of folds
+    start : int
+        start index
+    stop : int
+        stop index
+    step : int
+        step index
+    method : str
+        algorithm to run k_fold on
+        """
     kf = StratifiedKFold(n_splits=folds, shuffle = True)
     accuracy_mean_train = np.array([])
     accuracy_mean_test = np.array([])
@@ -14,38 +35,43 @@ def k_fold(X_data, y, folds, start, stop, step, method):
         accuracy_train = np.array([])
         accuracy_test = np.array([])
         for train, test in kf.split(X_data, y):
-            
             if(method == 'tree'):
-                split_clf = tree.DecisionTreeClassifier(criterion='gini', max_depth = i)
-                split_clf = split_clf.fit(X_data[train], y[train])
-                accuracy_train = np.append(
-                    accuracy_train, metrics.accuracy_score(y[train], split_clf.predict(X_data[train])))
-                accuracy_test = np.append(
-                    accuracy_test, metrics.accuracy_score(y[test], split_clf.predict(X_data[test]))) 
-            
+                clf = tree.DecisionTreeClassifier(criterion='gini', max_depth = i)
             elif(method == 'rf'):
-                rf = RandomForestClassifier(n_estimators = 100, criterion='gini', max_depth = i)
-                rf = rf.fit(X_data[train], y[train])
-                accuracy_train = np.append(
-                    accuracy_train, metrics.accuracy_score(y[train], rf.predict(X_data[train])))
-                accuracy_test = np.append(
-                    accuracy_test, metrics.accuracy_score(y[test], rf.predict(X_data[test]))) 
-                
+                clf = RandomForestClassifier(n_estimators = 100, criterion='gini', max_depth = i)
             elif(method == 'rf_estimators'):
-                    rf = RandomForestClassifier(n_estimators = i, criterion='gini', max_depth = 4)
-                    rf = rf.fit(X_data[train], y[train])
-                    accuracy_train = np.append(
-                        accuracy_train, metrics.accuracy_score(y[train], rf.predict(X_data[train])))
-                    accuracy_test = np.append(
-                        accuracy_test, metrics.accuracy_score(y[test], rf.predict(X_data[test]))) 
+                clf = RandomForestClassifier(n_estimators = i, criterion='gini', max_depth = 4)
+            elif(method == 'xgb'):
+                clf = xgb.XGBClassifier(n_estimators = 100, max_depth = i, eval_metric='error', use_label_encoder=False) 
+            elif(method == 'xgb_estimators'):
+                clf = xgb.XGBClassifier(n_estimators = i, max_depth = 5, eval_metric='error', use_label_encoder=False)
+            
+            clf = clf.fit(X_data[train], y[train])
+            accuracy_train = np.append(accuracy_train, metrics.accuracy_score(y[train], clf.predict(X_data[train])))
+            accuracy_test = np.append(accuracy_test, metrics.accuracy_score(y[test], clf.predict(X_data[test]))) 
                 
         accuracy_mean_train = np.append(accuracy_mean_train, np.mean(accuracy_train))
         accuracy_mean_test = np.append(accuracy_mean_test, np.mean(accuracy_test))
     return accuracy_mean_train, accuracy_mean_test
 
-
-
 def plot_classification_error(start, stop, step, accuracy_mean_train, accuracy_mean_test, function):
+    """
+    Returns accuracy mean for train and test data
+    Parameters
+    ----------
+    start : int
+        start index
+    stop : int
+        stop index
+    step : int
+        step index
+    accuracy_mean_train : numpy array
+        accuracy_mean values of train data
+    accuracy_mean_test : numpy array
+        accuracy_mean values of test data
+    function : str
+        function description
+        """
     plt.plot(range(start,stop, step), (1-accuracy_mean_train), label='accuracy train set')
     plt.plot(range(start,stop, step), (1-accuracy_mean_test), label='accuracy test set')
     plt.legend()
